d88d7e248d92e64a7e74d56dcc246fe8dc95958e
Angule Mathias <angulemathias3@gmail.com>
2018-07-24 16:19:53 +0300

    [FEATURE]#159086830_ diary models added

--- app/models.py
+++ app/models.py
@@ -1,32 +1,41 @@
-from datetime import datetime, timedelta
-from flask import current_app
-import jwt
+from datetime import datetime
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
-class DB():
-    '''Class for mock database'''
+
+class DB:
+    """Class for mock database"""
+
     def __init__(self):
         self.users = {}
         self.entries = {}
         self.user_count = 0
         self.entry_count = 0
-    
+
     def drop(self):
         self.__init__()
-    
+
+
 db = DB()
 
-class Base():
-    '''Base class to be inherited by User and Entry classes'''   
+
+class Base:
+    """Base class to be inherited by User and Entry classes"""
+
+    def __init__(self):
+        pass
+
     def update(self, data):
         # Validate keys before passing to data.
         for key in data:
             setattr(self, key, data[key])
-        setattr(self, 'last_modified', datetime.utcnow().isoformat())    
+        setattr(self, 'last_modified', datetime.utcnow().isoformat())
         return self.view()
 
+
 class User(Base):
-    '''Class to model user'''
+    """Class to model user"""
+
     def __init__(self, username, password, email):
         self.username = username
         self.password = generate_password_hash(password)
@@ -41,28 +50,15 @@ class User(Base):
         db.user_count += 1
         db.entries.update({self.id: {}})
         return self.view()
-    
+
     def validate_password(self, password):
         if check_password_hash(self.password, password):
             return True
         return False
-    
+
     def delete(self):
         del db.users[self.id]
-    
-    def generate_token(self):
-        payload = {'exp': datetime.utcnow()+timedelta(minutes=60),
-                    'iat': datetime.utcnow(),
-                    'username': self.username,
-                    'id': self.id}
-        token = jwt.encode(payload, str(current_app.config.get('SECRET')), algorithm='HS256')
-        return token.decode()
-    
-    @staticmethod
-    def decode_token(token):
-        payload = jwt.decode(token, str(current_app.config.get('SECRET')), algorithms=['HS256'])
-        return payload
-    
+
     def view(self):
         keys = ['username', 'email', 'id']
         return {key: getattr(self, key) for key in keys}
@@ -73,7 +69,7 @@ class User(Base):
         if not user:
             return {'message': 'User does not exist.'}
         return user
-    
+
     @classmethod
     def get_user_by_email(cls, email):
         for id_ in db.users:
@@ -81,7 +77,7 @@ class User(Base):
             if user.email == email:
                 return user
         return None
-    
+
     @classmethod
     def get_user_by_username(cls, username):
         for id_ in db.users:
@@ -89,9 +85,11 @@ class User(Base):
             if user.username == username:
                 return user
         return None
-   
+
+
 class Entry(Base):
-    '''Class to model entry'''
+    """Class to model entry"""
+
     def __init__(self, title, description, user_id):
         self.title = title
         self.description = description
@@ -103,19 +101,19 @@ class Entry(Base):
     def save(self):
         setattr(self, 'id', db.entry_count + 1)
         db.entry_count += 1
-        db.entries[self.user_id].update({self.id : self})
+        db.entries[self.user_id].update({self.id: self})
         return self.view()
-          
+
     def delete(self):
         del db.entries[self.user_id][self.id]
-    
+
     def view(self):
         keys = ('id', 'title', 'description', 'user_id', 'last_modified', 'created_at')
         return {key: getattr(self, key) for key in keys}
-    
+
     @classmethod
     def get(cls, user_id, id=None):
-        user_entries  = db.entries.get(user_id)
+        user_entries = db.entries.get(user_id)
         if not user_entries:
             return {'message': 'User does not have any entries'}
         if id:
