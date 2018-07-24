a29b68014352c6d6f62eb190d7cf82aa6ef6969e
Angule Mathias <angulemathias3@gmail.com>
2018-07-24 16:43:50 +0300

    [CHORE]#159271332_Diary user endpoints tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -17,7 +17,7 @@ def test_wrong_signup(self):
     """Test API cannot successfully register a new user if any field is left blank(POST request)"""
     response = self.client.post(SIGNUP_URL,
                                 data=json.dumps(
-                                    {'username': '', 'email': 'godipo@gmail.com', 'password': ''}),
+                                    {'username': 'angule', 'email': 'mathias@yahoo.com', 'password': ''}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 400)
     result = json.loads(response.data.decode())
@@ -39,7 +39,7 @@ def test_cannot_signup_with_invalid_username(self):
     """Test API cannot successfully register user if username invalid(POST request)"""
     response = self.client.post(SIGNUP_URL,
                                 data=json.dumps(
-                                    {'username': '#$gloria', 'email': 'godipo@gmail.com', 'password': 'passw'}),
+                                    {'username': '#$3454', 'email': 'shimanyiro@gmail.com', 'password': 'password'}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 400)
     result = json.loads(response.data.decode())
@@ -50,40 +50,18 @@ def test_cannot_signup_with_wrong_email_format(self):
     """Test API cannot successfully register user if email is invalid(POST request)"""
     response = self.client.post(SIGNUP_URL,
                                 data=json.dumps(
-                                    {'username': 'gloria', 'email': 'godipogmail.com', 'password': 'passw'}),
+                                    {'username': 'mathias', 'email': '12ma.com', 'password': 'password'}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 400)
     result = json.loads(response.data.decode())
     self.assertEqual(result["message"], "Invalid email. Ensure email is of the form example@mail.com")
 
 
-def test_cannot_signup_with_short_username(self):
-    """Test API cannot successfully register user if username is less than 4 char.(POST request)"""
-    response = self.client.post(SIGNUP_URL,
-                                data=json.dumps(
-                                    {'username': 'glo', 'email': 'godipo@gmail.com', 'password': 'password'}),
-                                content_type='application/json')
-    self.assertEqual(response.status_code, 400)
-    result = json.loads(response.data.decode())
-    self.assertEqual(result["message"], "Username should be atleast 4 characters")
-
-
-def test_cannot_signup_with_short_password(self):
-    """Test API cannot successfully register user if password is less than 8 char.(POST request)"""
-    response = self.client.post(SIGNUP_URL,
-                                data=json.dumps(
-                                    {'username': 'gloriao', 'email': 'godipo@gmail.com', 'password': 'pass'}),
-                                content_type='application/json')
-    self.assertEqual(response.status_code, 400)
-    result = json.loads(response.data.decode())
-    self.assertEqual(result["message"], "Password should be atleast 8 characters")
-
-
 def test_login(self):
     """Test API can successfully log in registered users using username and password (POST request)"""
     self.test_user.save()
     response = self.client.post(LOGIN_URL,
-                                data=json.dumps({'username': 'gloria', 'password': 'password'}),
+                                data=json.dumps({'username': 'mathias', 'password': 'password'}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 200)
     result = json.loads(response.data.decode())
@@ -94,7 +72,7 @@ def test_wrong_password(self):
     """Test API cannot authenticate login when wrong password is used (POST request)"""
     self.test_user.save()
     response = self.client.post(LOGIN_URL,
-                                data=json.dumps({'username': 'gloria', 'password': 'wrong_password'}),
+                                data=json.dumps({'username': 'mathias', 'password': 'wrong_password'}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 401)
     result = json.loads(response.data.decode())
@@ -104,7 +82,7 @@ def test_wrong_password(self):
 def test_login_nonexistent_user(self):
     """Test API cannot authenticate login when user is nonexistent (POST request)"""
     response = self.client.post(LOGIN_URL,
-                                data=json.dumps({'username': 'nonexistent', 'password': 'wrong_password'}),
+                                data=json.dumps({'username': 'nonexistent', 'password': 'nonexistent-password'}),
                                 content_type='application/json')
     self.assertEqual(response.status_code, 404)
     result = json.loads(response.data.decode())
