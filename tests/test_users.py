74f03fc10663ddd063293424055af002a0bf05ed
Angule Mathias <angulemathias3@gmail.com>
2018-08-01 15:05:46 +0300

    [FIX] Update travis

--- tests/test_users.py
+++ tests/test_users.py
@@ -15,17 +15,16 @@ class TestUser(BaseTestClass):
         data = json.loads(response.data)
         self.assertEqual(data[u'message'], 'User created successfully')
 
-#     def test_signup_invalid_email(self):
-#         response = self.client.post('/auth/signup',
-#                                     data=json.dumps({
-#                                         'username': 'Maureen',
-#                                         'email': 'nansubuga@gmail',
-#                                         'password': 'carol1234'}),
-#                                     headers={'content-type': 'application/json'})
-#         print(response.data)
-#         self.assertEqual(response.status_code, 400)
-#         data = json.loads(response.get_data())
-#         self.assertEqual(data['message'], 'Invalid email format.')
+    def test_signup_invalid_email(self):
+        response = self.client.post('/auth/signup',
+                                    data=json.dumps({
+                                        'username': 'Maureen',
+                                        'email': 'nansubuga@gmail',
+                                        'password': 'carol1234'}),
+                                    headers={'content-type': 'application/json'})
+        self.assertEqual(response.status_code, 400)
+        data = json.loads(response.data)
+        self.assertEqual(data[u'message'], 'Invalid email format.')
 #
 #     def test_signup_same_username(self):
 #         self.signup_user()
